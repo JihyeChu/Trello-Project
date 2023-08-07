521043bf410d458a2940296a164b4d910bc0e7f8
minjaejeong <jmj0531@naver.com>
2023-08-07 18:11:11 +0900

    User 클래스 구축 테스트 전 커밋

--- src/main/java/com/sparta/trelloproject/user/repository/UserRepository.java
+++ src/main/java/com/sparta/trelloproject/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.trelloproject.user.repository;
 
-public class UserRepository {
+import com.sparta.trelloproject.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+    Optional<User> findByUserName(String userName);
 }
