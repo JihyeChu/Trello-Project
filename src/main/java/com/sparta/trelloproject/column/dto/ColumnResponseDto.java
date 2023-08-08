352712f6faa0993c7baf59e0c31b96fa7e02363c
Ahn Jeongmin <133615790+MI-Ryeon@users.noreply.github.com>
2023-08-08 17:21:51 +0900

    [Feature] Column CURD

--- src/main/java/com/sparta/trelloproject/column/dto/ColumnResponseDto.java
+++ src/main/java/com/sparta/trelloproject/column/dto/ColumnResponseDto.java
@@ -1,4 +1,22 @@
 package com.sparta.trelloproject.column.dto;
 
+import com.sparta.trelloproject.column.entity.Column;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ColumnResponseDto {
+    private String columnName;
+    private Long boardId;
+//    private List<CardResponseDto> cardList;
+
+    public ColumnResponseDto(Column column) {
+        this.columnName = column.getColumnName();
+//        this.boardId = column.getBoard().getId();
+//        this.cardList = column.getCardList().stream()
+//                .map(ColumnResponseDto::new)
+//                .sorted(Comparator.comparing(ColumnResponseDto::getCreateAt))
+//                .toList();
+    }
 }
--- src/main/java/com/sparta/trelloproject/column/repository/ColumnRepository.java
+++ src/main/java/com/sparta/trelloproject/column/repository/ColumnRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.trelloproject.column.repository;
 
-public class ColumnRepository {
+import com.sparta.trelloproject.column.entity.Column;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ColumnRepository extends JpaRepository<Column, Long> {
+    Optional<Column> findByBoardIdAndColumnId(Long boardId, Long ColumnId);
 }
