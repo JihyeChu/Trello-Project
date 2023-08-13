e4fa34e8bd4743a762003ed9388d82c1a4ff3177
Ahn Jeongmin <133615790+MI-Ryeon@users.noreply.github.com>
2023-08-13 22:11:13 +0900

    [Fix] Column 조회 시 CardList 조회

--- src/main/java/com/sparta/trelloproject/column/dto/ColumnResponseDto.java
+++ src/main/java/com/sparta/trelloproject/column/dto/ColumnResponseDto.java
@@ -1,8 +1,13 @@
 package com.sparta.trelloproject.column.dto;
 
+import com.sparta.trelloproject.card.dto.CardNameResponseDto;
+import com.sparta.trelloproject.card.dto.CardResponseDto;
 import com.sparta.trelloproject.column.entity.ColumnEntity;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,15 +15,14 @@ public class ColumnResponseDto {
     private Long boardId;
     private String userName;
     private String columnName;
-//    private List<CardResponseDto> cardList;
+    private List<CardNameResponseDto> cardList;
 
     public ColumnResponseDto(ColumnEntity column) {
         this.columnName = column.getColumnName();
         this.boardId = column.getBoard().getId();
         this.userName = column.getUser().getUserName();
-//        this.cardList = column.getCardList().stream()
-//                .map(ColumnResponseDto::new)
-//                .sorted(Comparator.comparing(ColumnResponseDto::getCreateAt))
-//                .toList();
+        this.cardList = column.getCardList().stream()
+                .map(CardNameResponseDto::new)
+                .toList();
     }
 }
